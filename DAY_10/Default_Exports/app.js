f623e8e4a0bf8db67a18dcbf004028a558406095
shraddha goyal <shraddhagoyal800@gmail.com>
2023-10-11 16:40:39 +0530

    Default exports done

---
+++ DAY_10/Default_Exports/app.js
@@ -0,0 +1,15 @@
+// function can be imported in line or at the bottom both
+// there can be only 1 defautlt export
+
+// export default function greet (){
+//     console.log("greet")
+// }
+
+// default export (individually)
+// but the variable should be exportd in this way only
+// arrays can be exported like functions
+let arr = [1,2,3,4,5];
+export var a = "awesome";
+
+
+export default a; // export at the bottom 
---
+++ DAY_10/Default_Exports/index.js
@@ -0,0 +1,13 @@
+// default imports no need of curly braces because there is only 1 default export
+import a from "./app.js";
+// variable name can be of same name or changed name
+// import array from "./app.js"; // we change the name here then that name should be everywhere
+
+
+console.log(a);
+// console.log(array)
+
+
+
+
+
