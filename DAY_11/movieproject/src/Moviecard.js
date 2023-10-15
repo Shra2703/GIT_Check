3f37bcf43703f4ae7d20a2cf1c5eac90604555f4
shraddha goyal <shraddhagoyal800@gmail.com>
2023-10-15 18:20:10 +0530

    Toggling the favaourite button and add-to-cart is done

--- DAY_11/movieproject/src/Moviecard.js
+++ DAY_11/movieproject/src/Moviecard.js
@@ -11,14 +11,18 @@ class Moviecard extends React.Component {
       price: 199,
       rating: 8.9,
       starCount: 0,
+      fav: false,
+      cart: false,
     };
     // we can bind it here or bind when we are passing the refrence or we can use arrow function.
     this.incStars = this.incStars.bind(this);
   }
+
+  // increase the stars function
   incStars() {
     console.log("increase the count");
 
-    if(this.state.starCount >= 5){
+    if (this.state.starCount >= 5) {
       return;
     }
 
@@ -26,9 +30,9 @@ class Moviecard extends React.Component {
 
     // there are 2 forms to use setState() #form1
     this.setState({
-      starCount: this.state.starCount + 0.5
-    })
-    
+      starCount: this.state.starCount + 0.5,
+    });
+
     // 1) setState() do batching in #1 form it will batch them together and render only the last one it we do +2 then only that last will get rendered.
 
     // this.setState({
@@ -48,22 +52,24 @@ class Moviecard extends React.Component {
 
     // console.log("outside the callback", this.state.starCount)
   }
+
+  // decrease the stars function
   descStars = () => {
     console.log("decrease the count");
 
-    if(this.state.starCount <= 0){
+    if (this.state.starCount <= 0) {
       return;
     }
-    // #form 2 setState 
+    // #form 2 setState
     // used when we need to bother the previous state in the component
 
     this.setState((prevState) => {
-      return{
-        starCount: prevState.starCount - 0.5
-      }
-    })
+      return {
+        starCount: prevState.starCount - 0.5,
+      };
+    });
 
-     // 1) setState() do not do batching in #2 form it will call each and every setState() funct and render only once this happens because we are using previous state in the callback.
+    // 1) setState() do not do batching in #2 form it will call each and every setState() funct and render only once this happens because we are using previous state in the callback.
 
     // this.setState((prevState) => {
     //   return{
@@ -75,12 +81,26 @@ class Moviecard extends React.Component {
     //     starCount: prevState.starCount - 0.5
     //   }
     // })
-    
-    
   };
+
+  // handle the favourite btn
+  handleFavourite = () => {
+    this.setState({
+      fav: !this.state.fav,
+    });
+  };
+
+  // handle the favourite btn
+  handleCart = () => {
+    this.setState({
+      cart: !this.state.cart,
+    });
+  };
+
+  // main render function to render the HTML elments
   render() {
     // destructring the data
-    const { plot, price, rating, starCount } = this.state;
+    const { plot, price, rating, starCount, fav,cart } = this.state;
 
     return (
       <>
@@ -125,8 +145,33 @@ class Moviecard extends React.Component {
 
                   <span className="starCount">{starCount}</span>
                 </div>
-                <button className="favourite-btn">Favourite</button>
-                <button className="cart-btn">Add to Cart</button>
+
+                {/* #1 way to toggle the favourite and unfavourite button */}
+                {fav ? (
+                  <button
+                    className="unfavourite-btn"
+                    onClick={this.handleFavourite}
+                  >
+                    Un-Favourite
+                  </button>
+                ) : (
+                  <button
+                    className="favourite-btn"
+                    onClick={this.handleFavourite}
+                  >
+                    Favourite
+                  </button>
+                )}
+
+                {/* #2 way to toggling*/}
+                <button
+                  className={cart ? "unfavourite-btn" : "cart-btn"}
+                  onClick={this.handleCart}
+                >
+                  {cart ? "Remove from Cart" : "Add to Cart"}
+                </button>
+
+                
               </div>
             </div>
           </div>
