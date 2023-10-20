e2a3fcb75c77e503ad13581ea23b269156a1d4c6
shraddha goyal <shraddhagoyal800@gmail.com>
2023-10-20 18:44:44 +0530

    Techers data separate component is done

---
+++ DAY_18/codetube/src/TeachersData.js
@@ -0,0 +1,24 @@
+// teachers data
+
+export const teachers = [
+    {
+        img:"https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga311881_bill_gates.jpg",
+        title:"Bill Gates",
+        isFollow:false
+    },
+    {
+        img:"https://cdn.britannica.com/99/236599-050-1199AD2C/Mark-Zuckerberg-2019.jpg",
+        title:"Mark Zuckerberg",
+        isFollow:false
+    },
+    {
+        img:"https://people.com/thmb/o2uRKtWwmHpMVUzanSK7OKWICcI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(704x9:706x11)/satya-nadella-bd654c90995746879fb441cdbc284009.jpg",
+        title:"Satya Nadella",
+        isFollow:false
+    },
+    {
+        img:"https://cdn.britannica.com/42/193142-050-F69B1A23/Sundar-Pichai-Google.jpg",
+        title:"Sundar Pichai",
+        isFollow:false
+    }
+]
