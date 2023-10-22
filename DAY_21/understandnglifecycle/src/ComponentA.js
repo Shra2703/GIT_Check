d0f9256cd8879a4fc111c5d28986e78a97f90829
shraddha goyal <shraddhagoyal800@gmail.com>
2023-10-23 01:18:27 +0530

    learned life cycle methods and write the code for the responsiveness for codetube mini project

---
+++ DAY_21/understandnglifecycle/src/ComponentA.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class ComponentA extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Component A",
+      name2: "componenet A for test",
+    };
+    console.log("Constructor");
+    // we can't put setState here also because it is mounted yet.
+  }
+
+  static getDerivedStateFromProps() {
+    // cannot use setState here to 
+    let { name2 } = this.state;
+    console.log("Static");
+   
+    return null;
+  }
+
+  componentDidMount() {
+    // all the side effects used here only
+    console.log("didMount");
+  }
+
+  render() {
+    // we can't put this.setState here because setState() inplicitly calls an render() function so it will go on to the infinite state.
+    console.log("Render");
+    return (
+      <>
+        <h1>{this.state.name}</h1>
+        <h2>{this.state.name2}</h2>
+      </>
+    );
+  }
+}
+
+export default ComponentA;
