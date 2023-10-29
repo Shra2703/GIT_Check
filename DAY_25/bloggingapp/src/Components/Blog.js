532b4cc9dcdef63ebe94cc0b85018a1402c184f4
shraddha goyal <shraddhagoyal800@gmail.com>
2023-10-30 01:27:05 +0530

    updated the text and title with the objects

--- DAY_25/bloggingapp/src/Components/Blog.js
+++ DAY_25/bloggingapp/src/Components/Blog.js
@@ -2,19 +2,24 @@ import { useEffect, useState } from "react";
 
 export default function Blog() {
   // variables to store the content and the title
-  let [title, setTitle] = useState("");
-  let [text, setText] = useState("");
+  // let [title, setTitle] = useState("");
+  // let [text, setText] = useState("");
+
+  // one variable to update title and text both
+  let [formData, setFormdata] = useState({ title: "", text: "" });
   let [blogs, setBlogs] = useState([]);
 
   // this function will be invoked when submit button will be clicked
   function handleSubmit(e) {
     e.preventDefault();
-    setBlogs([{ title, text }, ...blogs]);
+    setBlogs([{ title: formData.title, text: formData.text }, ...blogs]);
     console.log(blogs);
 
     // to clear the text from the input fields
-    setTitle("");
-    setText("");
+    // setTitle("");
+    // setText("");
+
+    setFormdata({ title: "", text: "" });
   }
 
   // function which renders
@@ -42,8 +47,11 @@ export default function Blog() {
             type="text"
             className="input"
             placeholder="Enter the title here"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            value={formData.title}
+            // onChange={(e) => setTitle(e.target.value)}
+            onChange={(e) =>
+              setFormdata({ title: e.target.value, text: formData.text })
+            }
           />
           <hr />
           {/* <Row label="Content">
@@ -60,8 +68,11 @@ export default function Blog() {
           <textarea
             className="input content"
             placeholder="Content goes here..."
-            value={text}
-            onChange={(e) => setText(e.target.value)}
+            value={formData.text}
+            // onChange={(e) => setText(e.target.value)}
+            onChange={(e) =>
+              setFormdata({ title: formData.title, text: e.target.value })
+            }
           ></textarea>
           <hr />
 
@@ -76,8 +87,8 @@ export default function Blog() {
 
       {/* results will ne shown here */}
       <h2>Blogs</h2>
-      {blogs.map((blog) => (
-        <div className="blog" key={1}>
+      {blogs.map((blog, i) => (
+        <div className="blog" key={i}>
           <h3>{blog.title}</h3>
           <p>{blog.text}</p>
         </div>
