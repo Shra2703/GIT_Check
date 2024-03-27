6eb00387da21e81dc643a40cc193fa8d93e7da81
shraddha goyal <shraddhagoyal800@gmail.com>
2024-03-27 18:13:53 +0530

    items details page added in ecommerce website

--- DAY_11.2/reactrouting/src/Components/Navbar1.js
+++ DAY_11.2/reactrouting/src/Components/Navbar1.js
@@ -5,11 +5,43 @@ const Navbar1 = () => {
     <>
       {/* Link attribute will not style that when we are in that link (you are changing the page)*/}
       {/* <Link to="/">Home</Link> <br /> <br /> */}
-
-      <NavLink style = {({isActive}) => isActive === true ? {color : "pink"} : undefined} to="/">Home</NavLink> <br /> <br />
-      <NavLink style = {({isActive}) => isActive === true ? {color : "pink"} : undefined} to="/about">About</NavLink> <br /> <br />
-      <NavLink style = {({isActive}) => isActive === true ? {color : "pink"} : undefined} to="/contact">Contact Us</NavLink> <br /> <br />
-      <NavLink style = {({isActive}) => isActive === true ? {color : "pink"} : undefined} to="/items"> Items</NavLink> <br />
+      <NavLink
+        style={({ isActive }) =>
+          isActive === true ? { color: "pink" } : undefined
+        }
+        to="/"
+      >
+        Home
+      </NavLink>{" "}
+      <br /> <br />
+      <NavLink
+        style={({ isActive }) =>
+          isActive === true ? { color: "pink" } : undefined
+        }
+        to="/about"
+      >
+        About
+      </NavLink>{" "}
+      <br /> <br />
+      <NavLink
+        style={({ isActive }) =>
+          isActive === true ? { color: "pink" } : undefined
+        }
+        to="/contact"
+      >
+        Contact Us
+      </NavLink>{" "}
+      <br /> <br />
+      <NavLink
+        style={({ isActive }) =>
+          isActive === true ? { color: "pink" } : undefined
+        }
+        to="/items"
+      >
+        {" "}
+        Items
+      </NavLink>{" "}
+      <br />
       {/* by this outlet all the other component content will be seen on screen */}
       <Outlet />
     </>
